bbc1b43e1f90a9526c600ff668f83e7a1e423d56
YourtionGuo <yourtion@gmail.com>
2016-10-18 21:18:17 +0800

    update room function

--- Server/src/services/room.js
+++ Server/src/services/room.js
@@ -1,44 +1,56 @@
 'use strict';
 
 const logger = $.utils.createLogger('service:room');
+const mime = require('./mime');
+
 const room = {};
 
 room.createRoom = function (data, callback) {
-  // data = self.request.body
+  
+  logger.debug('createRoom');
+  logger.trace('input data: \n', data);
+
   let roomNo;
   if (data) {
-    // data = JSON.loads(data)
     roomNo = data.no;
   }
-
   const sid = data.sid;
   const key = $.utils.getRedisKey(roomNo);
+  logger.trace('roomNo: %s , sid: %s , key: %s ', roomNo, sid, key);
   $.redis.hgetall(key, (err, room) => {
+    if(err) return callback(err);
+
     if (room && room.cnt) { // 房间已经开了
+      logger.debug('has room');
       let userCnt = parseInt(room.cnt, 10);
       const mimeData = room.data;
+      logger.trace(' userCnt : %s \n mimeData: \n %s', userCnt, mimeData);
       if (userCnt < $.config.maxUserCount) {
         userCnt += 1; // TODO: 这里可以记录另一个玩家的信息
-        $.redis.hset(key, 'cnt', userCnt, (_err) => {
+        $.redis.hset(key, 'cnt', userCnt, (err) => {
+          if(err) return callback(err);
+
           const lefts = room.lefts[0];
-          const rspData = {
+          const result = {
             'map': mimeData,
             'count': lefts,
           };
-          // self.write(rspSuccess(rspData))
-          callback(null, rspData);
+          logger.trace(' lefts: %s \n result: \n %s', lefts, result);
+
+          return callback(null, result);
         });
 
       } else {
-        // self.write(rspError(u'房间%s已经满员' % roomNo))
-        return callback(new Error(`房间{$roomNo}已经满员`));
-        // self.finish()
-        // return
+        logger.trace('room %s is full', roomNo);
+        return callback(new Error(`房间${ roomNo }已经满员`));
       }
 
     } else { // 新房间
-      const mimeData = $.mime.genMimeArr();
-      const initData = $.mime.getInitArr(9);
+
+      logger.debug('new room');
+      const mimeData = mime.genMimeArr();
+      const initData = mime.getInitArr(9);
+
       const room = { // TODO: 这里可以记录用户的信息
         'createTime': parseInt(new Date() * 1000, 10),
         'cnt': 1,
@@ -49,17 +61,18 @@ room.createRoom = function (data, callback) {
         'curId': sid,
         'lastId': 0,
       };
-      // logger.trace(key ,room);
-      $.redis.hmset(key, room, (err, ret) => {
-        logger.trace(err, ret);
+
+      logger.trace('room info: \n key: %s \n room: \n %s', key, room);
+      $.redis.hmset(key, room, 60 * 60 * 12, (err) => { // 12小时
+        if(err) return callback(err);
+        
         const rspData = {
           'map': initData,
-          'count': 10, // mimeCnt
+          'count': mime.mimeCnt, // mimeCnt
         };
         logger.trace('room number: ', roomNo);
-        // self.write(rspSuccess(rspData))
-       return callback(null, rspData);
-      }); // 12小时
+        return callback(null, rspData);
+      });
 
     }
   });
