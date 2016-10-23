a55d72dedbfb9ad8f39ce6256561f737539cd955
YourtionGuo <yourtion@gmail.com>
2016-10-23 13:09:24 +0800

    remove Redis Command

--- Server/src/lib/redis.js
+++ Server/src/lib/redis.js
@@ -3,15 +3,4 @@
 const Redis = require('ioredis');
 const redis = new Redis($.config.redis);
 
-Redis.Command.setReplyTransformer('hgetall', function (result) {
-  if (Array.isArray(result)) {
-    const obj = {};
-    for (let i = 0; i < result.length; i += 2) {
-      obj[result[i]] = result[i + 1];
-    }
-    return obj;
-  }
-  return result;
-});
-
 module.exports = redis;
